913e6da583d29b25e08791156a0049bc661f5d5d
RyanCheungGit <ryanzjlib@gmail.com>
2014-07-23 10:47:54 +0800

    solved using reduce and xor

--- Single Number.py
+++ Single Number.py
@@ -2,8 +2,4 @@ class Solution:
     # @param A, a list of integer
     # @return an integer
     def singleNumber(self, A):
-        for i in range(len(A)):
-            try:
-                A[i:].remove(A[i])
-            except:
-                return A[i]
+        return reduce(operator.xor, A)
