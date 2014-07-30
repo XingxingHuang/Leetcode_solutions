55a1a804324ff0996258b87cd2ba67e8a3762792
ryancheunggit <ryanzjlib@gmail.com>
2014-07-30 17:47:31 +0800

    O(n) solution using dictionary

--- Two Sum.py
+++ Two Sum.py
@@ -1,5 +1,24 @@
+"""
+accepted solution O(n)
+iterate through the list once
+"""
+class Solution:
+    # @return a tuple, (index1, index2)
+    def twoSum(self, num, target):
+        dict = {}
+        for i, n in enumerate(num):
+            if target-n in dict:
+                return (dict[target-n]+1,i+1)
+            else:
+                dict[n] = i
+"""
+initial solution O(n^2) 
+
+
 class Solution:
     # @return a tuple, (index1, index2)
     def twoSum(self, num, target):
         sums = [(i+1,j+1) for i in range(len(num)) for j in range(i+1,len(num)) if num[i]+num[j] == target]
-        return sums[0][0],sums[0][1]
+        return sums[0]
+
+"""
