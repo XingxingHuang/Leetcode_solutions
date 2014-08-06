a08f64b4649de4dad2f30a837a3416b1532fa65c
Ryan Cheung <ryanzjlib@gmail.com>
2014-08-06 20:10:40 +0800

    Update Search Insert Position.py
    O(n)

--- Search Insert Position.py
+++ Search Insert Position.py
@@ -3,7 +3,26 @@ class Solution:
     # @param target, an integer to be inserted
     # @return integer
     def searchInsert(self, A, target):
+        B = [abs(x-target) for x in A]
+        m = min(B)
+        i = B.index(min(B))
+        if m == 0:
+            return i
+        elif A[i]-target >0:
+            return i
+        else:
+            return i+1
+        
+
+
+'''
+class Solution:
+    # @param A, a list of integers
+    # @param target, an integer to be inserted
+    # @return integer
+    def searchInsert(self, A, target):
         if target not in A:
             A.append(target)
             A.sort()
         return A.index(target)
+'''
