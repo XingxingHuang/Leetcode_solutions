e253801ea9f5cb7051707bf8c6c7f3b76ae3e609
Ren Zhang <ryanzjlib@gmail.com>
2018-02-03 23:17:35 -0500

    Update solution_ver1.py

--- algorithms/779_k-th_Symbol_in_Grammar/solution_ver1.py
+++ algorithms/779_k-th_Symbol_in_Grammar/solution_ver1.py
@@ -7,24 +7,6 @@ class Solution:
         :type K: int
         :rtype: int
         """
-        # too slow
-        # mapping = {
-        #         "0": "01",
-        #         "1": "10"
-        #     }
-        # rows = ["0"]
-        # for i in range(N-1):
-        #     rows.append("".join([mapping[element] for element in rows[-1]]))
-        # return int(rows[-1][K-1])
-        # also slow
-        # mapping = {
-        #     "0": "1",
-        #     "1": "0"
-        # }
-        # rows = ["0"]
-        # for i in range(N-1):
-        #     rows.append(rows[-1] + "".join([mapping[element] for element in rows[-1]]))
-        # return int(rows[-1][K-1])
         # use recursive!
         if N == 1 and K == 1:
             return 0
@@ -36,13 +18,12 @@ class Solution:
 
 
 def run():
-    Solution().findWords(["Hello", "Alaska", "Dad", "Peace"])
+    Solution().kthGrammar(4,5)
 
 def main():
     timer = timeit.Timer('run()', "from __main__ import run")
     t = timer.timeit(1000)
     print(t)
-    print(Solution().findWords(["Hello", "Alaska", "Dad", "Peace"]))
 
 if __name__ == '__main__':
     main()
