d1f19950ca973afcbec72c057d58b92241150ed4
ryancheunggit <ryanzjlib@gmail.com>
2018-02-17 21:51:37 -0500

    784 using generator

---
+++ algorithms/784_Letter_Case_Permutation/solution_ver1.py
@@ -0,0 +1,31 @@
+#
+import timeit
+
+class Solution:
+    def letterCasePermutation(self, S):
+        """
+        :type S: str
+        :rtype: List[str]
+        """
+        return list(self._permutation_generator(S))
+
+    def _permutation_generator(self, S):
+        if S == '':
+            yield ''
+            return
+        for rest in self._permutation_generator(S[1:]):
+            yield S[0].upper() + rest
+            if S[0].upper() != S[0].lower():
+                yield S[0].lower() + rest
+
+
+def run():
+    Solution().letterCasePermutation("a1b2")
+
+def main():
+    timer = timeit.Timer('run()', "from __main__ import run")
+    t = timer.timeit(1000)
+    print(t)
+
+if __name__ == '__main__':
+    main()
