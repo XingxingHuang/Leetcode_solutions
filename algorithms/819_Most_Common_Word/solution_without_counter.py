61b0b7760c526ae822c947e966c29fe8273421d1
ryancheunggit <ryanzjlib@gmail.com>
2018-06-03 19:07:43 -0400

    an alternative not using Counter,

---
+++ algorithms/819_Most_Common_Word/solution_without_counter.py
@@ -0,0 +1,19 @@
+class Solution:
+    def mostCommonWord(self, paragraph, banned):
+        """
+        :type paragraph: str
+        :type banned: List[str]
+        :rtype: str
+        """
+        paragraph = "".join(char.lower() for char in paragraph if char not in "!?',;.")
+        # words = [word for word in paragraph.split(" ") if word not in banned]
+        # return Counter(words).most_common(1)[0][0]
+        word_counts = {}
+        for word in paragraph.split(" "):
+            if word in banned:
+                continue
+            if word not in word_counts:
+                word_counts[word] = 1
+            else:
+                word_counts[word] += 1
+        return sorted(word_counts.items(), key=lambda kv: kv[1], reverse=True)[0][0]
